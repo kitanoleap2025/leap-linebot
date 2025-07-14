07f2c65a0002f5f6b871accf3099c02aaa8bcbe3
kitanoleap2025 <applesoda3100@gmail.com>
2025-07-14 22:07:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -45,5 +45,8 @@ def handle_message(event):
             TextSendMessage(text=question)
         )
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
