21c253767e7ffefbeecfee7f5f06072714c4a2c3
kitanoleap2025 <applesoda3100@gmail.com>
2025-07-16 22:22:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -118,8 +118,10 @@ questions = [
      "answer": "construction"},
     {"text": "610 I can ___ everything except temptation.\n私は誘惑以外の全てに耐えうる.",
      "answer": "resist"},
-    {"text": "155 \n",
-     "answer": ""},
+    {"text": "782 ___ woman\n熟女",
+     "answer": "mature"},
+    {"text": "1000 ___ __ capitakism, your value peaks at checkout.\n資本主義によると,あなたの価値はチェックアウト時にピークに達する.",
+     "answer": "according to"}
 ]
 
 @app.route("/callback", methods=["POST"])
