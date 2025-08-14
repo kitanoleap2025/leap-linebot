bc9e0729b4e91a2a573a560446fa52311de7acee
kitanoleap2025 <applesoda3100@gmail.com>
2025-08-14 17:17:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -449,7 +449,7 @@ def build_result_flex(user_id):
                     "layout": "vertical",
                     "contents": [],
                     "backgroundColor": color_map[rank],
-                    "flex": max(1, int(rank_ratios[rank]*1)),
+                    "flex": max(1, int(rank_ratios[rank]*20)),
                     "height": "12px"
                 }
             ],
