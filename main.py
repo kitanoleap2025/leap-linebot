a34502f024839ab4e1b55afaf20758e4ba01862d
矢吹匡 <applesoda3100@gmail.com>
2025-09-15 15:15:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -540,7 +540,6 @@ def handle_message_common(event, bot_type, line_bot_api):
         quick_buttons = [
             QuickReplyButton(action=MessageAction(label="1-1000", text="1-1000")),
             QuickReplyButton(action=MessageAction(label="1001-2000", text="1001-2000")),
-            QuickReplyButton(action=MessageAction(label="間違えた問題", text="0%"))
         ]
         line_bot_api.reply_message(
             event.reply_token,
