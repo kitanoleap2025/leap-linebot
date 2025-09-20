4963bc667eb43e61e5adc5678383496579fabc4f
矢吹匡 <applesoda3100@gmail.com>
2025-09-20 23:30:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -649,16 +649,6 @@ def handle_message_common(event, bot_type, line_bot_api):
         
         messages_to_send = [flex_feedback]
 
-        # スロット
-        grid = spin_slot()
-        flex_message = build_slot_flex(grid)
-        messages_to_send = [flex_feedback, flex_message]
-
-        if user_answer_counts[user_id] % 5 == 0:
-            async_save_user_data(user_id)
-            trivia = random.choice(trivia_messages)
-            messages_to_send.append(TextSendMessage(text=trivia))
-
         # 次の問題
         next_question_msg = send_question(user_id, range_str, bot_type=bot_type)
         messages_to_send.append(next_question_msg)
