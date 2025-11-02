d7c51f5dcfaf850df01d944a3b7419a86b1e0b19
矢吹匡 <applesoda3100@gmail.com>
2025-11-02 21:10:37 +0900

    Update main.py

--- api/main.py
+++ api/main.py
@@ -3,20 +3,16 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from linebot.exceptions import InvalidSignatureError
 
-# LINE SDK 初期化
 line_bot_api = LineBotApi(os.environ["LINE_CHANNEL_ACCESS_TOKEN_LEAP"])
 handler = WebhookHandler(os.environ["LINE_CHANNEL_SECRET_LEAP"])
 
-# LINEイベント処理
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    # 受け取ったテキストをそのまま返すだけ
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=f"あなたは「{event.message.text}」と言いました")
     )
 
-# Vercelサーバーレス用エントリーポイント
 def handler_main(request):
     if request.method != "POST":
         return "Method Not Allowed", 405
