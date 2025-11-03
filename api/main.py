f7563fdb027cfc196f5517e221b18d66ed8a1d7b
矢吹匡 <applesoda3100@gmail.com>
2025-11-03 18:20:18 +0900

    Update main.py

--- api/main.py
+++ api/main.py
@@ -3,30 +3,39 @@ import os
 from linebot import LineBotApi, WebhookHandler
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from linebot.exceptions import InvalidSignatureError
+from http.server import BaseHTTPRequestHandler
+import json
 
 line_bot_api = LineBotApi(os.environ["LINE_CHANNEL_ACCESS_TOKEN_LEAP"])
 handler = WebhookHandler(os.environ["LINE_CHANNEL_SECRET_LEAP"])
 
-# LINEメッセージイベントの処理
+def handler(request):
+    # POST 以外は許可しない
+    if request.method != "POST":
+        return {
+            "statusCode": 405,
+            "body": "Method Not Allowed"
+        }
+
+    body = request.get_data(as_text=True)
+    signature = request.headers.get("x-line-signature", "")
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        return {
+            "statusCode": 400,
+            "body": "Invalid signature"
+        }
+
+    return {
+        "statusCode": 200,
+        "body": "OK"
+    }
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    # 受け取ったメッセージをそのまま返信
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=f"受け取りました: {event.message.text}")
     )
-
-# Vercelサーバーレス関数のエントリポイント
-def handler(request):
-    if request.method != "POST":
-        return "Method Not Allowed", 405
-
-    body = request.get_data(as_text=True)
-    signature = request.headers.get("X-Line-Signature", "")
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        return "Invalid signature", 400
-
-    # 5秒以内にOKを返す
-    return "OK", 200
