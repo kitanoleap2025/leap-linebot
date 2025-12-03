af84bd7cf7bc2f1ae40bedf6c325c4775e89ed3d
矢吹匡 <applesoda3100@gmail.com>
2025-12-03 20:35:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ def get_rank(score):
     return {0: "✖", 1: "✔/❓", 2: "✔2", 3: "✔3", 4: "✔4"}.get(score, "✔/❓")
 
 def score_to_weight(score):
-    return {0: 100000, 1: 1000, 2:1000, 3: 100, 4: 1}.get(score, 1000000000)
+    return {0: 100000, 1: 1000, 2:1000, 3: 100, 4: 1}.get(score, 100000000000)
 
 def build_result_flex(user_id, bot_type):
     name = user_names.get(user_id, DEFAULT_NAME)
