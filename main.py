ba846ded1e1816f14bbb281384f748601959e8ae
矢吹匡 <applesoda3100@gmail.com>
2025-12-28 23:12:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -54,40 +54,6 @@ user_streaks = defaultdict(int)
 user_daily_e = defaultdict(lambda: {"date": None, "total_e": 0})
 user_fever = defaultdict(int)  # user_id: 0 or 1
 user_ranking_wait = defaultdict(int)  # user_id: 残りカウント
-#---------------------------------------------------------------------------------
-parser = WebhookParser(os.getenv("LINE_CHANNEL_SECRET_LEAP"))
-
-handler_leap = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET_LEAP"))
-
-@app.route("/callback/leap", methods=["POST"])
-def callback_leap():
-    body = request.get_data(as_text=True)
-    signature = request.headers.get("X-Line-Signature", "")
-    if not signature:
-        abort(400, "Missing X-Line-Signature")
-    try:
-        handler_leap.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400, "Invalid signature")
-    return "OK"
-
-# LEAP イベントハンドラ
-@handler_leap.add(MessageEvent, message=TextMessage)
-def handle_leap_message(event):
-    handle_message_common(event, bot_type="LEAP", line_bot_api=line_bot_api_leap)
-
-@app.route("/health")
-def health():
-    ua = request.headers.get("User-Agent", "")
-    if "cron-job.org" in ua:
-        return "ok", 200
-    return "unauthorized", 403
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port)
-
-#---------------------------------------------------------------------------------
     
 def fever_time(fevertime):
     # fevertime が None または 0 のとき
@@ -850,3 +816,38 @@ def handle_message_common(event, bot_type, line_bot_api):
         event.reply_token,
         TextSendMessage(text="「学ぶ」を押してみましょう！")
     )
+
+#---------------------------------------------------------------------------------
+parser = WebhookParser(os.getenv("LINE_CHANNEL_SECRET_LEAP"))
+
+handler_leap = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET_LEAP"))
+
+@app.route("/callback/leap", methods=["POST"])
+def callback_leap():
+    body = request.get_data(as_text=True)
+    signature = request.headers.get("X-Line-Signature", "")
+    if not signature:
+        abort(400, "Missing X-Line-Signature")
+    try:
+        handler_leap.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400, "Invalid signature")
+    return "OK"
+
+# LEAP イベントハンドラ
+@handler_leap.add(MessageEvent, message=TextMessage)
+def handle_leap_message(event):
+    handle_message_common(event, bot_type="LEAP", line_bot_api=line_bot_api_leap)
+
+@app.route("/health")
+def health():
+    ua = request.headers.get("User-Agent", "")
+    if "cron-job.org" in ua:
+        return "ok", 200
+    return "unauthorized", 403
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
+
+#---------------------------------------------------------------------------------
