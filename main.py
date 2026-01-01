9868f34b13448e2017959e49db3f12097d051b9c
矢吹匡 <applesoda3100@gmail.com>
2026-01-01 17:31:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -613,17 +613,14 @@ def handle_message(event):
 def handle_message_common(event, line_bot_api):
     user_id = event.source.user_id
     msg = event.message.text.strip()
-
-    data = load_user_from_firestore(user_id)
-    # メモリへ展開
-    user_scores[user_id] = defaultdict(lambda: 1, data.get("scores", {}))
-    user_names[user_id] = data.get("name", DEFAULT_NAME)
-
     user_daily_e[user_id]["total_e"] = data.get("total_e", 0)
     user_daily_e[user_id]["date"] = data.get("total_e_date")
 
     if user_id not in user_states:
+        data = load_user_from_firestore(user_id)
         load_user_data(user_id)
+        user_scores[user_id] = defaultdict(lambda: 1, data.get("scores", {}))
+        user_names[user_id] = data.get("name", DEFAULT_NAME)
     
     # 名前変更コマンド
     if msg.startswith("@"):
